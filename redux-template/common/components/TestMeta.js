cf2e66925e339b0efe884ab1803350cceda68256
Andrea Caldera <andrea.caldera@gmail.com>
2017-04-16 11:48:18 +0100

    more counter cleanup, displaying active toggle in test page

--- redux-template/common/components/TestMeta.js
+++ redux-template/common/components/TestMeta.js
@@ -1,29 +1,43 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import _ from 'lodash';
 
 import meta from '../modules/meta';
 
-// TODO redux-form?
-
-const TestMeta = ({ testMeta, setTestMeta, saveTestMeta }) => (
-  <form onSubmit={setTestMeta}>
+const TestMeta = ({ testMeta, setTestMeta, saveTestMeta, toggles }) => {
+  // TODO remove null / empty values from toggles
+  const toggleList = _.isEmpty(toggles) ?
+    (<p>No toggles selected</p>) :
+    (<div>
+      <p>Active toggles:</p>
+      <ul>
+        {toggles.map((toggle) => (<li key={`${toggle}-item`}>{toggle}</li>))}
+      </ul>
+    </div>);
+  return (
     <div>
-      <label htmlFor="testMeta">Test  meta</label>
-      <input name="testMeta" placeholder="testMeta" onBlur={setTestMeta} />
-    </div>
-    <button onClick={saveTestMeta}>Set test meta</button>
-    <div>{testMeta}</div>
-  </form>
-);
+      {toggleList}
+      <form onSubmit={setTestMeta}>
+        <div>
+          <label htmlFor="testMeta">Test  meta</label>
+          <input name="testMeta" placeholder="testMeta" onBlur={setTestMeta} />
+        </div>
+        <button onClick={saveTestMeta}>Set test meta</button>
+        <div>{testMeta}</div>
+      </form>
+    </div>);
+};
 
 TestMeta.propTypes = {
+  toggles: PropTypes.array.isRequired,
   setTestMeta: PropTypes.func.isRequired,
   testMeta: PropTypes.string.isRequired,
   saveTestMeta: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = (state) => ({
+  toggles: meta.getToggles(state),
   testMeta: meta.getTestMeta(state),
 });
 
--- redux-template/common/containers/App.js
+++ redux-template/common/containers/App.js
@@ -1,20 +1,4 @@
-// import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux';
-// import Counter from '../components/Counter'
-// import * as CounterActions from '../actions'
-
 import TestMeta from '../components/TestMeta';
-// import metaSelectors from '../modules/meta/selectors'
-// import metaActions from '../modules/meta/actions';
-
-// const mapStateToProps = (state) => ({
-// });
-//
-// const mapDispatchToProps = (dispatch) => ({
-// });
-
-// function mapDispatchToProps(dispatch) {
-//   return bindActionCreators(CounterActions, dispatch)
-// }
 
 export default connect()(TestMeta);

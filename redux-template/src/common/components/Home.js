a592ece396f4d6ac8213c08aff9837a193a2a16e
Andrea Caldera <andrea.caldera@gmail.com>
2017-11-01 12:59:02 +0000

    display element when at the bottom of the page

--- redux-template/src/common/components/Home.js
+++ redux-template/src/common/components/Home.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+import uiModule from '../modules/ui';
 
 import FeatureTogglesComponent from './FeatureToggles';
 
 const FeatureToggles = React.createFactory(FeatureTogglesComponent);
 
-const Home = () =>
+const Home = ({ hasReachedPageBottom }) =>
 (<div>
   <h1>Home</h1>
   {FeatureToggles()}
+  {hasReachedPageBottom && (<div className="home__bottom">You have reached the abyss of this page</div>)}
 </div>);
 
-export default connect()(Home);
+Home.propTypes = {
+  hasReachedPageBottom: PropTypes.bool.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  hasReachedPageBottom: uiModule.getHasReachedPageBottom(state),
+});
+
+export default connect(mapStateToProps, null)(Home);
